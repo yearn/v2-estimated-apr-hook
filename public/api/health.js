6f45bb337d2543862c1e511cf4acc316ff82076c
matheus1lva <matheus.g.silva@hotmail.com>
2026-08-12 09:49:15 -0300

    fix: drop accidental public/ build artifacts from infisical PR
    Keep deploy.yml + next.config Infisical migration only.

--- public/api/health.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = handler;
-async function handler(_req, res) {
-    res.status(200).send('ok');
-}
--- public/src/types/kongTypes.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// Types derived from live GraphQL introspection of https://kong.yearn.farm/api/gql
-Object.defineProperty(exports, "__esModule", { value: true });
